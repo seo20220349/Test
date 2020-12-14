8944ace54b974ff5c0a1ebf3072cbda5cf31b39f
이재원 <nani6765@gmail.com>
2020-12-14 16:40:37 +0000

    none export

--- js/main.js
+++ js/main.js
@@ -1,79 +1,38 @@
-const begin = () => {
-  header.style.animation =
-    'going-up 0.4s forwards, ' +
-    'fade-out 0.4s forwards';
-  footer.style.animation =
-    'going-down 0.4s forwards, ' +
-    'fade-out 0.4s forwards';
-  setTimeout(() => main.style.animation =
-    'going-up 0.4s ease-in-out forwards, ' +
-    'fade-out 0.4s ease-in-out forwards', 500);
-  setTimeout(() => {
-    header.style.display = 'none';
-    footer.style.display = 'none';
-    main.style.display = 'none';
-    qna.style.display = 'block';
-    if (pcMQL.matches) {
-      console.log('PC');
-      wrap.style.marginTop = '50px';
-    } else if (tabletMQL.matches) {
-      console.log('tablet');
-      wrap.style.marginTop = '30px';
-    }
-    goNext();
-  }, 1000);
-}
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+const qna = document.getElementById('qna');
+const u_name = document.querySelector('input[type=text]');
+const wrap = document.getElementById('wrap');
+const tabletMQL = window.matchMedia("all and (min-width: 768px)");
+const pcMQL = window.matchMedia("all and (min-width: 1024px)");
+const ENDPOINT = 10;
+const select = [];
+let qIdx = -1;
 
-const goNext = () => {
-  if (qIdx++ === qnaList.length - 1) {
-    end();
-    return;
+const calcScore = () => {
+  let point = 0;
+  for (let i = 0; i < ENDPOINT; i++) {
+    point += qnaList[i].a[select[i]].score;
   }
-
-  const status = document.querySelector('.status');
-  const qNum = qnaList[qIdx];
-  const q = document.querySelector('.q');
-
-  status.style.width = (ENDPOINT * (qIdx + 1)) + '%';
-  q.innerHTML = qNum.q;
-  qna.style.animation =
-    'fade-in 0.3s ease-in-out 0.4s forwards, ' +
-    'going-down 0.3s ease-in-out 0.4s forwards';
-
-  setTimeout(() => {
-    const endIdx = qNum.a.length - 1;
-    for (let i in qNum.a) {
-      addAnswer(qNum.a[i].answer, i);
-    }
-    qna.style.opacity = 1;
-  }, 700);
+  return point;
 }
 
-const end = () => {
-  qna.style.animation = '';
-  const interval = setInterval(() => {
-    qna.style.opacity -= 0.1;
-    qna.style.transform = 'translateY(-1px)';
-  }, 50);
-  setTimeout(() => clearTimeout(interval), 500);
-  setTimeout(() => qna.style.display = 'none', 500);
-  setTimeout(() => {
-    const calc = document.getElementById('calc');
-    calc.style.display = 'block';
-    calc.style.animation =
-      'going-up 0.5s forwards, ' +
-      'fade-in 0.5s forwards';
-  }, 700);
-  setTimeout(() => {
-    calc.style.animation = '';
-    calc.style.animation =
-      'going-left 0.4s forwards, ' +
-      'fade-out 0.4s forwards';
-    setTimeout(() => {
-      calc.style.display = 'none';
-      goResult();
-    }, 400);
-  }, 9000);
+const sortResult = (point) => {
+  let num = 0;
+  if (point <= 20) {
+    num = 0;
+  } else if (point <= 30) {
+    num = 1;
+  } else if (point <= 40) {
+    num = 2;
+  } else if (point <= 50) {
+    num = 3;
+  } else if (point <= 60) {
+    num = 4;
+  } else {
+    num = 5;
+  }
+  return num;
 }
 
 const goResult = () => {
@@ -122,4 +81,138 @@ const goResult = () => {
 
 }
 
-export { begin, goNext, end, goResult };
+const end = () => {
+  qna.style.animation = '';
+  const interval = setInterval(() => {
+    qna.style.opacity -= 0.1;
+    qna.style.transform = 'translateY(-1px)';
+  }, 50);
+  setTimeout(() => clearTimeout(interval), 500);
+  setTimeout(() => qna.style.display = 'none', 500);
+  setTimeout(() => {
+    const calc = document.getElementById('calc');
+    calc.style.display = 'block';
+    calc.style.animation =
+      'going-up 0.5s forwards, ' +
+      'fade-in 0.5s forwards';
+  }, 700);
+  setTimeout(() => {
+    calc.style.animation = '';
+    calc.style.animation =
+      'going-left 0.4s forwards, ' +
+      'fade-out 0.4s forwards';
+    setTimeout(() => {
+      calc.style.display = 'none';
+      goResult();
+    }, 400);
+  }, 9000);
+}
+
+const addAnswer = (answerTxt, idx) => {
+  const answer = document.createElement('button');
+  const a = document.querySelector('.answer');
+  answer.className += 'a box';
+  answer.innerHTML = answerTxt;
+  answer.addEventListener('click', () => {
+    const parent = answer.parentNode;
+    const children = parent.childNodes;
+    for (let i in children) {
+      children[i].disabled = true;
+    }
+    parent.classList.add('fade-out-5-4');
+    setTimeout(() => {
+      select[qIdx] = idx;
+      a.innerHTML = '';
+      parent.classList.remove('fade-out-5-4');
+      goNext();
+    }, 800);
+  });
+
+  setTimeout(() => answer.style.animation =
+    'going-down 0.25s forwards, fade-in 0.25s forwards', 50);
+  a.appendChild(answer);
+}
+
+
+const goNext = () => {
+  if (qIdx++ === qnaList.length - 1) {
+    end();
+    return;
+  }
+
+  const status = document.querySelector('.status');
+  const qNum = qnaList[qIdx];
+  const q = document.querySelector('.q');
+
+  status.style.width = (ENDPOINT * (qIdx + 1)) + '%';
+  q.innerHTML = qNum.q;
+  qna.style.animation =
+    'fade-in 0.3s ease-in-out 0.4s forwards, ' +
+    'going-down 0.3s ease-in-out 0.4s forwards';
+
+  setTimeout(() => {
+    const endIdx = qNum.a.length - 1;
+    for (let i in qNum.a) {
+      addAnswer(qNum.a[i].answer, i);
+    }
+    qna.style.opacity = 1;
+  }, 700);
+}
+
+const begin = () => {
+  const main = document.getElementById('main');
+  header.style.animation =
+    'going-up 0.4s forwards, ' +
+    'fade-out 0.4s forwards';
+  footer.style.animation =
+    'going-down 0.4s forwards, ' +
+    'fade-out 0.4s forwards';
+  setTimeout(() => main.style.animation =
+    'going-up 0.4s ease-in-out forwards, ' +
+    'fade-out 0.4s ease-in-out forwards', 500);
+  setTimeout(() => {
+    header.style.display = 'none';
+    footer.style.display = 'none';
+    main.style.display = 'none';
+    qna.style.display = 'block';
+    if (pcMQL.matches) {
+      console.log('PC');
+      wrap.style.marginTop = '50px';
+    } else if (tabletMQL.matches) {
+      console.log('tablet');
+      wrap.style.marginTop = '30px';
+    }
+    goNext();
+  }, 1000);
+}
+
+export const load = () => {
+  const msg = document.querySelector('.check-name');
+  const start_btn = document.querySelector('.start');
+
+  u_name.addEventListener('blur', () => {
+    try {
+      if (u_name.value.length < 1) {
+        throw '이름을 입력하고 시작해 주세요.';
+      }
+      msg.innerHTML = '';
+    } catch (err) {
+      msg.innerHTML = err;
+    }
+  });
+
+  start_btn.addEventListener('click', () => {
+    try {
+      if (u_name.value.length < 1) {
+        throw '이름을 입력하고 시작해 주세요.';
+      }
+      msg.innerHTML = '';
+      start_btn.disabled = true;
+      begin();
+    } catch (err) {
+      msg.innerHTML = err;
+    }
+  });
+}
+
+window.onload = load();
