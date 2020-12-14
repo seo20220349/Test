9c917ac1dc9b0f650c565aa393d942a1d1f56469
이재원 <nani6765@gmail.com>
2020-12-14 16:14:16 +0000

    change js name

--- js/start.js
+++ js/start.js
@@ -1,52 +1,45 @@
-const begin = () => {
-  header.style.animation =
-    'going-up 0.4s forwards, ' +
-    'fade-out 0.4s forwards';
-  footer.style.animation =
-    'going-down 0.4s forwards, ' +
-    'fade-out 0.4s forwards';
-  setTimeout(() => main.style.animation =
-    'going-up 0.4s ease-in-out forwards, ' +
-    'fade-out 0.4s ease-in-out forwards', 500);
-  setTimeout(() => {
-    header.style.display = 'none';
-    footer.style.display = 'none';
-    main.style.display = 'none';
-    qna.style.display = 'block';
-    if (pcMQL.matches) {
-      console.log('PC');
-      wrap.style.marginTop = '50px';
-    } else if (tabletMQL.matches) {
-      console.log('tablet');
-      wrap.style.marginTop = '30px';
+import { begin, goNext } from './main.js'
+
+const u_name = document.querySelector('input[type=text]');
+const main = document.getElementById('main');
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+const qna = document.getElementById('qna');
+const wrap = document.getElementById('wrap');
+const tabletMQL = window.matchMedia("all and (min-width: 768px)");
+const pcMQL = window.matchMedia("all and (min-width: 1024px)");
+
+const ENDPOINT = 10;
+const select = [];
+let qIdx = -1;
+
+const load = () => {
+  const msg = document.querySelector('.check-name');
+  const start_btn = document.querySelector('.start');
+
+  u_name.addEventListener('blur', () => {
+    try {
+      if (u_name.value.length < 1) {
+        throw '이름을 입력하고 시작해 주세요.';
+      }
+      msg.innerHTML = '';
+    } catch (err) {
+      msg.innerHTML = err;
     }
-    goNext();
-  }, 1000);
+  });
+
+  start_btn.addEventListener('click', () => {
+    try {
+      if (u_name.value.length < 1) {
+        throw '이름을 입력하고 시작해 주세요.';
+      }
+      msg.innerHTML = '';
+      start_btn.disabled = true;
+      begin();
+    } catch (err) {
+      msg.innerHTML = err;
+    }
+  });
 }
 
-const goNext = () => {
-  if (qIdx++ === qnaList.length - 1) {
-    end();
-    return;
-  }
-
-  const status = document.querySelector('.status');
-  const qNum = qnaList[qIdx];
-  const q = document.querySelector('.q');
-
-  status.style.width = (ENDPOINT * (qIdx + 1)) + '%';
-  q.innerHTML = qNum.q;
-  qna.style.animation =
-    'fade-in 0.3s ease-in-out 0.4s forwards, ' +
-    'going-down 0.3s ease-in-out 0.4s forwards';
-
-  setTimeout(() => {
-    const endIdx = qNum.a.length - 1;
-    for (let i in qNum.a) {
-      addAnswer(qNum.a[i].answer, i);
-    }
-    qna.style.opacity = 1;
-  }, 700);
-}
-
-export { begin, goNext };
+window.onload = load();
