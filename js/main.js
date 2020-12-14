b42b77d2b763f6328c9019ec28759fc32a89fa91
이재원 <nani6765@gmail.com>
2020-12-14 16:10:43 +0000

    test

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const main = document.getElementById('main');
 const header = document.getElementById('header');
 const footer = document.getElementById('footer');
 const qna = document.getElementById('qna');
+const wrap = document.getElementById('wrap');
+
+const tabletMQL = window.matchMedia("all and (min-width: 768px)");
+const pcMQL = window.matchMedia("all and (min-width: 1024px)");
 
 const begin = () => {
   header.style.animation =
