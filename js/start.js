35606a4087d44c8891552fd4aa96b4eb8ec60917
이재원 <nani6765@gmail.com>
2020-12-18 15:28:42 +0000

    sort

--- js/start.js
+++ js/start.js
@@ -55,10 +55,10 @@ const calcScore = () => {
 	
 	let resultArray = pointArray.sort(function(a, b){
 		if( a.value > b.value){
-			return 1;
+			return -1;
 		}
 		if (a.value < b.value){
-			return -1;
+			return 1;
 		}
 		return 0;
 	});
